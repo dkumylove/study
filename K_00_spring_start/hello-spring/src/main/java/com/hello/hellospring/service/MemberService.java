f6f67666ca9e7f672f4b228581edb4380bdd34b1
dkumylove <dkumylove@gmail.com>
2024-03-19 19:22:19 +0900

    03 : 회원관리 : 회원 서비스개발

--- K_00_spring_start/hello-spring/src/main/java/com/hello/hellospring/service/MemberService.java
+++ K_00_spring_start/hello-spring/src/main/java/com/hello/hellospring/service/MemberService.java
@@ -16,8 +16,11 @@ public class MemberService {
 
     private final MemberRepository memberRepository = new
             MemoryMemberRepository();
+
     /**
      * 회원가입
+     * @param member
+     * @return
      */
     public Long join(Member member) {
         validateDuplicateMember(member); //중복 회원 검증
@@ -38,8 +41,10 @@ public class MemberService {
                     throw new IllegalStateException("이미 존재하는 회원입니다.");
                 });
     }
+
     /**
      * 전체 회원 조회
+     * @return
      */
     public List<Member> findMembers() {
         return memberRepository.findAll();
@@ -47,6 +52,8 @@ public class MemberService {
 
     /**
      * 회원 조회
+     * @param memberId
+     * @return
      */
     public Optional<Member> findOne(Long memberId) {
         return memberRepository.findById(memberId);
