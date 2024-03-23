a4ee6fab460793c979e5fb4cc53c9ec0a9f75e68
dkumylove <dkumylove@gmail.com>
2024-03-24 01:17:07 +0900

    06: 스프링 DB접근기술 : 순수 JDBC

--- K_00_spring_start/hello-spring/src/main/java/com/hello/hellospring/SpringConfig.java
+++ K_00_spring_start/hello-spring/src/main/java/com/hello/hellospring/SpringConfig.java
@@ -1,20 +1,29 @@
 package com.hello.hellospring;
 
+import com.hello.hellospring.repository.JdbcMemberRepository;
 import com.hello.hellospring.repository.MemberRepository;
-import com.hello.hellospring.repository.MemoryMemberRepository;
 import com.hello.hellospring.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private final DataSource dataSource;
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
     }
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 
     /**
@@ -28,5 +37,19 @@ public class SpringConfig {
      * @Autowired 를 통한 DI는
      * - 스프링이 관리하는 객체에서만 동작한다.
      * - 스프링 빈으로 등록하지 않고 내가 직접 생성한 객체에서는 동작하지 않는다.
+     */
+
+    /**
+     * 개방-폐쇄 원칙(OCP, Open-Closed Principle)
+     * 확장에는 열려있고, 수정, 변경에는 닫혀있다.
+     *
+     * 스프링의 DI (Dependencies Injection)을 사용하면 기존 코드를 전혀 손대지 않고,
+     * 설정만으로 구현 클래스를 변경할 수 있다.
+     */
+
+    /**
+     * DataSource는 데이터베이스 커넥션을 획득할 때 사용하는 객체다.
+     * 스프링 부트는 데이터베이스 커넥션 정보를 바탕으로 DataSource를 생성하고
+     * 스프링 빈으로 만들어둔다. 그래서 DI를 받을 수 있다.
      */
 }
