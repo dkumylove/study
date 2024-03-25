476b4615555bea41c04c79b5843f6c69138f137d
dkumylove <dkumylove@gmail.com>
2024-03-25 23:48:21 +0900

    07: AOP : AOP가 필요한 상황

--- K_00_spring_start/hello-spring/src/main/java/com/hello/hellospring/service/MemberService.java
+++ K_00_spring_start/hello-spring/src/main/java/com/hello/hellospring/service/MemberService.java
@@ -42,9 +42,20 @@ public class MemberService {
      * @return
      */
     public Long join(Member member) {
-        validateDuplicateMember(member); //중복 회원 검증
-        memberRepository.save(member);
-        return member.getId();
+
+        // MemberService 회원 조회 시간 측정 추가
+        long start = System.currentTimeMillis();
+
+        try {
+            validateDuplicateMember(member); //중복 회원 검증
+            memberRepository.save(member);
+            return member.getId();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("join : " + timeMs + "ms");
+        }
+
     }
 
     /**
@@ -66,7 +77,17 @@ public class MemberService {
      * @return
      */
     public List<Member> findMembers() {
-        return memberRepository.findAll();
+
+        // MemberService 회원 조회 시간 측정 추가
+        long start = System.currentTimeMillis();
+        try {
+            return memberRepository.findAll();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("findMembers " + timeMs + "ms");
+        }
+
     }
 
     /**
@@ -78,6 +99,20 @@ public class MemberService {
         return memberRepository.findById(memberId);
     }
 
+
+    /**
+     * 07: AOP : AOP가 필요한 상황
+     * MemberService 회원 조회 시간 측정 추가 문제>>
+     *  - 회원가입, 회원 조회에 시간을 측정하는 기능은 핵심 관심 사항이 아니다.
+     *  - 시간을 측정하는 로직은 공통 관심 사항이다.
+     *  - 시간을 측정하는 로직과 핵심 비즈니스의 로직이 섞여서 유지보수가 어렵다.
+     *  - 시간을 측정하는 로직을 별도의 공통 로직으로 만들기 매우 어렵다.
+     *  - 시간을 측정하는 로직을 변경할 때 모든 로직을 찾아가면서 변경해야 한다.
+     *
+     *  AOP가 필요한 상황
+     *  - 모든 메소드의 호출 시간을 측정하고 싶다면?
+     *  - 공통 관심 사항(cross-cutting concern) vs 핵심 관심 사항(core concern)
+     */
 
     /**
      * 단축키
