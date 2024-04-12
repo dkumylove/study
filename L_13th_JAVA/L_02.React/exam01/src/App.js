1ae85dbdfdbca45f28d03a628c65214478c32289
dkumylove <dkumylove@gmail.com>
2024-04-13 02:21:34 +0900

    함수형컴포넌트) Counter 값출력위해 코드 수정

--- L_13th_JAVA/L_02.React/exam01/src/App.js
+++ L_13th_JAVA/L_02.React/exam01/src/App.js
@@ -1,11 +1,13 @@
-import MyComponent from './components/MyComponent2';
+//import MyComponent from './components/MyComponent2';
+import Counter from './components/Couter';
 
 const App = () => {
-  return (
-    <MyComponent name="이이름" num={3}>
-      <h1>반값습니다.</h1>
-    </MyComponent>
-    );
+  return <Counter />;
+  // return (
+  //   <MyComponent name="이이름" num={3}>
+  //     <h1>반값습니다.</h1>
+  //   </MyComponent>
+  // );
 };
 
 export default App;
