647068ac77e78c3bb3201157c395a7998dd5eb11
dkumylove <dkumylove@gmail.com>
2024-04-12 18:48:06 +0900

    컴포넌트) props 숫자추가

--- L_13th_JAVA/L_02.React/exam01/src/components/MyComponent.js
+++ L_13th_JAVA/L_02.React/exam01/src/components/MyComponent.js
@@ -1,6 +1,11 @@
-const MyComponent = ({ name }) => {
+const MyComponent = ({ name, num }) => {
 
-    return <div>안녕하세요!, 제 이름은 {name}입니다.</div>
+    return (
+        <>
+            <div>안녕하세요!, 제 이름은 {name}입니다.</div>
+            <div>좋아하는 숫자는 {num}입니다.</div>
+        </>
+    );
 };
 
 export default MyComponent;
