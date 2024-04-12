bb01b4b0f2ca397dc3c829f52b4ea507ef7e14e5
dkumylove <dkumylove@gmail.com>
2024-04-13 03:36:49 +0900

    컴포넌트) Profile/ useState -> changeProfile

--- L_13th_JAVA/L_02.React/exam01/src/App.js
+++ L_13th_JAVA/L_02.React/exam01/src/App.js
@@ -1,8 +1,14 @@
 //import MyComponent from './components/MyComponent2';
-import Counter from './components/Counter2';
+// import Counter from './components/Counter2';
+import Profile from './components/Profile';
 
 const App = () => {
-  return <Counter />;
+
+  return <Profile />;
+  /*
+  let num = 15;
+  return <Counter num={num} />;
+  */
   // return (
   //   <MyComponent name="이이름" num={3}>
   //     <h1>반값습니다.</h1>
---
+++ L_13th_JAVA/L_02.React/exam01/src/components/Profile.js
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+
+const Profile = () => {
+  const [data, setData] = useState({
+    name: '이이름',
+    age: 40,
+  });
+
+  const changeProfile = () => {
+    // data.name = '김이름';  // 주소값이 같으면 같은걸로 보기떄문에 렌더링 안됨
+    //data.age = 30;
+    setData({ name: '김이름', age: 30 });
+  };
+
+  const { name, age } = data;
+  return (
+    <>
+      <h1>{name}</h1>
+      <h2>{age}</h2>
+      <button type="button" onClick={changeProfile}>
+        변경
+      </button>
+    </>
+  );
+};
+
+export default Profile;
