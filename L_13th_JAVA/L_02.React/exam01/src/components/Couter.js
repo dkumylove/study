94cfeb504094d7dfb2e969a39e3a9b11b183b256
dkumylove <dkumylove@gmail.com>
2024-04-13 02:16:45 +0900

    함수형컴포넌트) Counter 리턴값

--- L_13th_JAVA/L_02.React/exam01/src/components/Couter.js
+++ L_13th_JAVA/L_02.React/exam01/src/components/Couter.js
@@ -2,6 +2,18 @@ import { useState } from 'react';
 
 const Counter = () => {
     const [number, setNumber] = useState(0);
+
+    return (
+        <>
+          <h1>{number}</h1>
+          <button type="button" onClick={setNumber(number + 1)}>
+            +1
+          </button>
+          <button type="button" onClick={setNumber(number - 1)}>
+            -1
+          </button>
+        </>
+      );
 };
 
 export default Counter;
