2e08d96a3d7fea4c6623250e82982403c0b80edc
dkumylove <dkumylove@gmail.com>
2024-04-13 01:38:51 +0900

    컴포넌트) children 속성

--- L_13th_JAVA/L_02.React/exam01/src/App.js
+++ L_13th_JAVA/L_02.React/exam01/src/App.js
@@ -1,7 +1,11 @@
 import MyComponent from './components/MyComponent';
 
 const App = () => {
-  return <MyComponent />;
+  return (
+    <MyComponent>
+      <h1>안녕하세요.</h1>
+    </MyComponent>
+    );
 };
 
 export default App;
