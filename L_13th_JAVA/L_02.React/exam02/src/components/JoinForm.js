15ef382f570d3d935786afc88b5e79662e1d0feb
dkumylove <dkumylove@gmail.com>
2024-04-19 21:49:07 +0900

    JoinForm.js

---
+++ L_13th_JAVA/L_02.React/exam02/src/components/JoinForm.js
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+
+const JoinForm = () => {
+    const [userId, setUserId] = useState('');
+    const [userPw, setUserPw] = useState('');
+
+    const onUserIdChange = (e) => setUserId(e.currentTarget.value);
+
+    return (
+      <form>
+        <dl>
+          <dt>아이디</dt>
+          <dd>
+            <input type="text" onChange={onUserIdChange} />
+          </dd>
+        </dl>
+        <dl>
+          <dt>비밀번호</dt>
+          <dd>
+            <input type="password" />
+          </dd>
+        </dl>
+        <dl>
+          <dt>비밀번호 확인</dt>
+          <dd>
+            <input type="password" />
+          </dd>
+        </dl>
+        <dl>
+          <dt>회원명</dt>
+          <dd>
+            <input type="text" />
+          </dd>
+        </dl>
+        <button type="submit">가입하기</button>
+      </form>
+    );
+  };
+  
+  export default JoinForm;
