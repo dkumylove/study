74eb3aefd3d35b0d12ae6962b97a14f3ac336bfc
dkumylove <dkumylove@gmail.com>
2024-04-19 20:45:48 +0900

    화살표함수) 상태값 변경

--- L_13th_JAVA/L_02.React/exam02/src/components/Color.js
+++ L_13th_JAVA/L_02.React/exam02/src/components/Color.js
@@ -5,20 +5,24 @@ class Color extends Component {
     color: 'blue',
   };
 
-  changeColor(color) {
-    console.log(this);
-  }
-
   render() {  // 컴포넌트의 필수
+
+    // function changeColor(color) {
+    //     console.log(this);
+    // }
+    
+    // const changeColor = (color) => console.log(this);  // Color
+    const changeColor = (color) => this.setState({ color }); // Color
+
     const { color } = this.state;
     return (
       <>
         <div
           style={{ background: color, width: '100px', height: '100px' }}
         ></div>
-        <button type="button" onClick={this.changeColor}>RED</button>
-        <button type="button">ORANGE</button>
-        <button type="button">GREEN</button>
+        <button type="button" onClick={() => changeColor('red')}>RED</button>
+        <button type="button" onClick={() => changeColor('orange')}>ORANGE</button>
+        <button type="button" onClick={() => changeColor('green')}>GREEN</button>
       </>
     );
   }
