1d17f5e719256d4326b1feaa35f309e92c731a89
dkumylove <dkumylove@gmail.com>
2024-04-19 21:02:19 +0900

    이벤트핸들링) onContestMenu

--- L_13th_JAVA/L_02.React/exam02/src/components/Color.js
+++ L_13th_JAVA/L_02.React/exam02/src/components/Color.js
@@ -3,6 +3,7 @@ import { Component } from 'react';
 class Color extends Component {
   state = { // 상태값
     color: 'blue',
+    subColor: 'black',
   };
 
   render() {  // 컴포넌트의 필수
@@ -13,16 +14,19 @@ class Color extends Component {
     
     // const changeColor = (color) => console.log(this);  // Color
     const changeColor = (color) => this.setState({ color }); // Color
+    const changeSubColor = (color) => this.setState((prevState) => ({ ...prevState, subColor: color }));
 
-    const { color } = this.state;
+
+    const { color, subColor } = this.state;
     return (
       <>
         <div
-          style={{ background: color, width: '100px', height: '100px' }}
-        ></div>
-        <button type="button" onClick={() => changeColor('red')}>RED</button>
-        <button type="button" onClick={() => changeColor('orange')}>ORANGE</button>
-        <button type="button" onClick={() => changeColor('green')}>GREEN</button>
+          style={{ background: color, width: '100px', height: '100px' }}></div>
+        <button type="button" onClick={() => changeColor('red')} onContextMenu={() => changeSubColor('red')}>RED</button>
+        <button type="button" onClick={() => changeColor('orange')} onContextMenu={() => changeSubColor('orange')}>ORANGE</button>
+        <button type="button" onClick={() => changeColor('green')} onContextMenu={() => changeSubColor('green')}>GREEN</button>
+
+        <div style={{ background: subColor, width: '50px', height: '50px' }}></div>
       </>
     );
   }
